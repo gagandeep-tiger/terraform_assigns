0e451bafb1f17e4ab956148d34eca6408345c1fb
gagandeeo <gagandeepprasad50@gmail.com>
2022-11-15 17:56:40 +0530

    tags-update

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -17,8 +17,6 @@ resource "azurerm_resource_group" "mle-cloud-training" {
   name = "mle-cloud-training"
   location = var.location
   tags = {
-        " created_by": "anitha.srinivasa"
-        " created_for": "MLE Cloud Training"
-        "POC": "Sheik Dawood, Bhagirathi Hedge, Senganal"
+        "key": "value"
     }
 }
